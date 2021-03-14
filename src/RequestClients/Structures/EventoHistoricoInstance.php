669f6a9dc8949de3d9bcd79f6fe3d10f7e3d478c
Felipe Figueroa <amenadiel@gmail.com>
2021-03-14 19:10:12 -0300

    ensures rutResponsable is a string

--- src/RequestClients/Structures/EventoHistoricoInstance.php
+++ src/RequestClients/Structures/EventoHistoricoInstance.php
@@ -11,58 +11,58 @@ namespace CTOhm\SiiAsyncClients\RequestClients\Structures;
 use InvalidArgumentException;
 use JsonSerializable;
 
- /**
-  * Structure to ensure normalization and type hinting of a single event.
-  *
-  * @property string $codEvento
-  * @property string $descEvento
-  * @property string $dvResponsable
-  * @property string $rutResponsable
-  */
- class EventoHistoricoInstance implements JsonSerializable
- {
-     public string $descEvento;
+/**
+ * Structure to ensure normalization and type hinting of a single event.
+ *
+ * @property string $codEvento
+ * @property string $descEvento
+ * @property string $dvResponsable
+ * @property string $rutResponsable
+ */
+class EventoHistoricoInstance implements JsonSerializable
+{
+    public string $descEvento;
 
-     public string $codEvento;
+    public string $codEvento;
 
-     public string $fechaEvento;
+    public string $fechaEvento;
 
-     public string $rutResponsable;
+    public string $rutResponsable;
 
-     private array $_mandatoryFields = ['descEvento', 'codEvento', 'fechaEvento', 'rutResponsable'];
+    private array $_mandatoryFields = ['descEvento', 'codEvento', 'fechaEvento', 'rutResponsable'];
 
-     /**
-      * Undocumented function.
-      *
-      * @param array{descEvento: string, codEvento: string, fechaEvento: string, rutResponsable: string, dvResponsable?: string} $eventArray
-      */
-     public function __construct(array $eventArray)
-     {
-         // ensure constructor argument has mandatory fields (only dvResponsable is optional)
-         foreach ($this->_mandatoryFields as $fieldName) {
-             if (!\array_key_exists($fieldName, $eventArray)) {
-                 throw new InvalidArgumentException(\sprintf('Evento inválido, no tiene campo %s', $fieldName));
-             }
-         }
-         // rutResponsable is the concatenation of rutResponsable-rutResponsable if rutResponsable is present, or rutResponsable otherwise
-         $this->rutResponsable = ($eventArray['dvResponsable'] ?? null) ?
+    /**
+     * Undocumented function.
+     *
+     * @param array{descEvento: string, codEvento: string, fechaEvento: string, rutResponsable: string, dvResponsable?: string} $eventArray
+     */
+    public function __construct(array $eventArray)
+    {
+        // ensure constructor argument has mandatory fields (only dvResponsable is optional)
+        foreach ($this->_mandatoryFields as $fieldName) {
+            if (!\array_key_exists($fieldName, $eventArray)) {
+                throw new InvalidArgumentException(\sprintf('Evento inválido, no tiene campo %s', $fieldName));
+            }
+        }
+        // rutResponsable is the concatenation of rutResponsable-rutResponsable if rutResponsable is present, or rutResponsable otherwise
+        $this->rutResponsable = strval(($eventArray['dvResponsable'] ?? null) ?
             \implode('-', [$eventArray['rutResponsable'], $eventArray['dvResponsable']])
-            : $eventArray['rutResponsable'];
-         $this->descEvento = $eventArray['descEvento'];
-         $this->codEvento = $eventArray['codEvento'];
-         $this->fechaEvento = $eventArray['fechaEvento'];
-     }
+            : $eventArray['rutResponsable']);
+        $this->descEvento = $eventArray['descEvento'];
+        $this->codEvento = $eventArray['codEvento'];
+        $this->fechaEvento = $eventArray['fechaEvento'];
+    }
 
-     /**
-      * Canonica array representation of this event.
-      */
-     public function jsonSerialize()
-     {
-         return [
-             'rutResponsable' => $this->rutResponsable,
-             'descEvento' => $this->descEvento,
-             'codEvento' => $this->codEvento,
-             'fechaEvento' => $this->fechaEvento,
-         ];
-     }
- }
+    /**
+     * Canonica array representation of this event.
+     */
+    public function jsonSerialize()
+    {
+        return [
+            'rutResponsable' => $this->rutResponsable,
+            'descEvento' => $this->descEvento,
+            'codEvento' => $this->codEvento,
+            'fechaEvento' => $this->fechaEvento,
+        ];
+    }
+}
